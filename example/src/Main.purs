3b3c3e945c9cfeaa16d2302be925aaa186501f69
Konstantin Zudov <konstantin@anche.no>
2015-09-09 03:51:09 +0300

    Added example

---
+++ example/src/Main.purs
@@ -0,0 +1,41 @@
+module Main where
+
+import Prelude
+import Data.Maybe
+import Control.Bind
+import Control.Monad
+import Control.Monad.Eff
+import Control.Monad.Eff.Var
+import Control.Monad.Eff.Console
+import Data.Either.Unsafe
+import Data.Foreign
+import Data.Foreign.Index
+import DOM.Event.EventTarget
+
+import WebSocket
+
+main = do
+  Connection socket <- newWebSocket "ws://echo.websocket.org" []
+
+  socket.onopen $= eventListener \event -> do
+    log "onopen: Connection opened"
+
+    log =<< get socket.url
+
+    log "onopen: Sending 'hello'"
+    socket.send "hello"
+
+    log "onopen: Sending 'goodbye'"
+    socket.send "goodbye"
+
+  socket.onmessage $= eventListener \event -> do
+    -- TODO: Figure out how to access 'data' properly, using MessageEvent
+    let message = unsafeFromForeign $ fromRight $ prop "data" $ toForeign event
+
+    log $ "onmessage: Received '" ++ message ++ "'"
+
+    when (message == "goodbye") do
+      log "onmessage: closing connection"
+      socket.close Nothing Nothing
+
+  socket.onclose $= eventListener \event -> log "onclose: Connection closed"
